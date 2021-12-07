1b968bae39fce32328de2738830efc52f7101f43
yuriymikhailidi <yumi4478@colorado.edu>
2021-12-07 00:46:28 -0700

    Set up, user interface, config parse

---
+++ dfserver/dfserver.c
@@ -0,0 +1,36 @@
+//
+// Created by Yuriy Mikhaildi on 12/3/21.
+//
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+
+#define BUFSIZE 1024
+
+typedef struct Client{
+    int sockfd; /* socket */
+    int portno; /* port to listen on */
+    int clientlen; /* byte size of client's address */
+    struct sockaddr_in serveraddr; /* server's addr */
+    struct sockaddr_in clientaddr; /* client addr */
+    struct hostent *hostp; /* client host info */
+
+}Client;
+
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+    perror(msg);
+    exit(0);
+}
+int main(int argc, char **argv){
+
+}
